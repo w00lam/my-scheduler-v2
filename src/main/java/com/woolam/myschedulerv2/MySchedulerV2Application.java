e16838dbb19cd00038e415800574e6b6b7a978b3
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-16 15:16:43 +0900

    Initial commit

---
+++ src/main/java/com/woolam/myschedulerv2/MySchedulerV2Application.java
@@ -0,0 +1,13 @@
+package com.woolam.myschedulerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchedulerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySchedulerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/woolam/myschedulerv2/MySchedulerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woolam.myschedulerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchedulerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
